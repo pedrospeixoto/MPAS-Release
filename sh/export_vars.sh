52dd65a272577eb0f516f9f885f6fc5bbcd0540c
Pedro Peixoto <pedrospeixoto1@gmail.com>
2015-10-28 20:47:07 +0000

    Update export_vars.sh

--- sh/export_vars.sh
+++ sh/export_vars.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
-BASEDIR=/scratch/pd300/MPASprogs
-SOURCEDIR=${BASEDIR}/sources
+export BASEDIR=/scratch/pd300/MPASprogs
+#export BASEDIR=/scratch/pedrosp
+
+export MPI_PATH=${BASEDIR}/openmpi
+export NETCDF_PATH=${BASEDIR}/netcdf
+export PNETCDF_PATH=${BASEDIR}/pnetcdf
+export PIO_PATH=${BASEDIR}/pio
+export METIS_PATH=${BASEDIR}/metis
 
 #clear all modules
 module purge
+
+#For ?
 #module load openmpi-x86_64
 #module load openmpi-gcc-4.8.2/1.6.5 
+
+#For emps-snadbach
 module load gcc/4.8.2 
+
+#For lince
+#module load openmpi/1.8.3-intel 
+
+#export FC=ifort
+#export F77=ifort
+#export F90=ifort
+#export CC=icc
 export FC=gfortran
 export F77=gfortran
 export F90=grortran
 export CC=gcc
 export MPIFC=mpif90
 export MPIF90=mpif90
-#not sure about this!!
 export MPIF77=mpif77
 export MPICC=mpicc
 
-#MPI stuff
-export MPI_PATH=${BASEDIR}/openmpi
+#MPI 
 export LD_LIBRARY_PATH=${MPI_PATH}/lib:$LD_LIBRARY_PATH
 export PATH=${MPI_PATH}/bin:$PATH
 
 #NETCDF and PIO
-export NETCDF_PATH=${BASEDIR}/netcdf
-export PNETCDF_PATH=${BASEDIR}/pnetcdf
-export PIO_PATH=${BASEDIR}/pio
-
 export NETCDF=$NETCDF_PATH
 export PNETCDF=$PNETCDF_PATH
 export PIO=$PIO_PATH
@@ -40,17 +52,16 @@ export PATH=${PNETCDF_PATH}/bin:$PATH
 export LD_LIBRARY_PATH=${PIO_PATH}/lib:$LD_LIBRARY_PATH
 export PATH=${PIO_PATH}/bin:$PATH
 
+#cmake - needed for metis
+#export PATH=${BASEDIR}/bin:$PATH
 
-#cmake
-export PATH=${BASEDIR}/bin:$PATH
-
-# METIS
+# METIS - use as: gpmetis graph.info N
 export PATH=${BASEDIR}/metis-5.1.0/build/Linux-x86_64/programs:$PATH
+#export LD_LIBRARY_PATH=${METIS_PATH}/lib:$LD_LIBRARY_PATH
+#export PATH=${METIS_PATH}/bin:$PATH
 
 #NCAR GRAPHICS NCL
 export PATH=${BASEDIR}/ncl_ncarg-6.3.0.Linux_RHEL6.4_x86_64_nodap_gcc472/bin:$PATH
-
-
 export NCARG_ROOT=/scratch/pd300/MPASprogs/ncl_ncarg-6.3.0.Linux_RHEL6.4_x86_64_nodap_gcc472
 
 
