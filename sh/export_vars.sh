ef3c07a0567da68d522534513ac878ec6e9ea209
Pedro da Silva Peixoto <pedrosp@ime.usp.br>
2015-11-18 15:26:30 +0000

    export_vars

--- sh/export_vars.sh
+++ sh/export_vars.sh
@@ -1,69 +1,95 @@
 #!/bin/bash
+# Last modif Peixoto Nov 2015
+# Comment what you don't need 
 
-export BASEDIR=/scratch/pd300/MPASprogs
-#export BASEDIR=/scratch/pedrosp
+#Lince
+export BASEDIR=/scratch/pedrosp
 
+#MAC cluster
+export BASEDIR=/scratch/pr63so/di25coq
+
+#CFD
+export BASEDIR=/var/tmp/pedrosp/MPAS
+
+#Laptop
+export BASEDIR=/opt
+
+export SOURCEDIR=${BASEDIR}/sources
 export MPI_PATH=${BASEDIR}/openmpi
 export NETCDF_PATH=${BASEDIR}/netcdf
 export PNETCDF_PATH=${BASEDIR}/pnetcdf
 export PIO_PATH=${BASEDIR}/pio
 export METIS_PATH=${BASEDIR}/metis
 
-#clear all modules
-module purge
 
-#For ?
+#clear all modules
+#module purge
+
 #module load openmpi-x86_64
 #module load openmpi-gcc-4.8.2/1.6.5 
 
-#For emps-snadbach
-module load gcc/4.8.2 
 
 #For lince
 #module load openmpi/1.8.3-intel 
 module load openmpi/1.10.1-intel
 
-#export FC=ifort
-#export F77=ifort
-#export F90=ifort
-#export CC=icc
+#For MAC cluster
+module load netcdf/mpi/4.3
+module load pnetcdf/1.6
+export NETCDF_PATH=${NETCDF_BASE}
+export PNETCDF_PATH=${PNETCDF_BASE}
+export PIO_PATH=${BASEDIR}/pio
+
+
+#For gnu
+module load gcc/4.8.2 
 export FC=gfortran
 export F77=gfortran
 export F90=grortran
 export CC=gcc
+
+#For intel
+export FC=ifort
+export F77=ifort
+export F90=ifort
+export CC=icc
+
+#MPI
 export MPIFC=mpif90
 export MPIF90=mpif90
 export MPIF77=mpif77
 export MPICC=mpicc
 
-#MPI 
-export LD_LIBRARY_PATH=${MPI_PATH}/lib:$LD_LIBRARY_PATH
-export PATH=${MPI_PATH}/bin:$PATH
-
-#NETCDF and PIO
+#For MPAS
 export NETCDF=$NETCDF_PATH
 export PNETCDF=$PNETCDF_PATH
 export PIO=$PIO_PATH
 
-#LIBs and PATHS
+
+export LD_LIBRARY_PATH=${MPI_PATH}/lib:$LD_LIBRARY_PATH
+export PATH=${MPI_PATH}/bin:$PATH
+
 export LD_LIBRARY_PATH=${NETCDF_PATH}/lib:$LD_LIBRARY_PATH
 export PATH=${NETCDF_PATH}/bin:$PATH
+
 export LD_LIBRARY_PATH=${PNETCDF_PATH}/lib:$LD_LIBRARY_PATH
 export PATH=${PNETCDF_PATH}/bin:$PATH
+
 export LD_LIBRARY_PATH=${PIO_PATH}/lib:$LD_LIBRARY_PATH
 export PATH=${PIO_PATH}/bin:$PATH
 
-#cmake - needed for metis
-#export PATH=${BASEDIR}/bin:$PATH
+export LD_LIBRARY_PATH=${METIS_PATH}/lib:$LD_LIBRARY_PATH
+export PATH=${METIS_PATH}/bin:$PATH
 
-# METIS - use as: gpmetis graph.info N
-export PATH=${BASEDIR}/metis-5.1.0/build/Linux-x86_64/programs:$PATH
-#export LD_LIBRARY_PATH=${METIS_PATH}/lib:$LD_LIBRARY_PATH
-#export PATH=${METIS_PATH}/bin:$PATH
-
-#NCAR GRAPHICS NCL
 export NCL_PATH=${BASEDIR}/ncl
 export PATH=${NCL_PATH}/bin:$PATH
 export LD_LIBRARY_PATH=${NCL_PATH}/lib:$LD_LIBRARY_PATH
 export NCARG_ROOT=${NCL_PATH}
+
+
+
+
+
+
+
 
